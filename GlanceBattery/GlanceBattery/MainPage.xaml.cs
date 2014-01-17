9c70332fad97c70824a011cbef23b9dd102e5b10
Tuan Tran Van <cuoilennaocacban2@hotmail.com>
2014-01-17 15:48:33 +0000

    background agent
    git-tfs-id: [https://cuoilennaocacban.visualstudio.com/]$/30 - Glance Battery;C1250

--- GlanceBattery/GlanceBattery/MainPage.xaml.cs
+++ GlanceBattery/GlanceBattery/MainPage.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿#define DEBUG_AGENT
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using Windows.Phone.Devices.Power;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Scheduler;
 using Microsoft.Phone.Shell;
 using GlanceBattery.Resources;
 
@@ -13,6 +17,9 @@ namespace GlanceBattery
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        PeriodicTask periodicTask { get; set; }
+
         // Constructor
         public MainPage()
         {
@@ -20,6 +27,13 @@ namespace GlanceBattery
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            BatteryPercentTextBlock.Text = Battery.GetDefault().RemainingChargePercent.ToString();
         }
 
         // Sample code for building a localized ApplicationBar
@@ -45,6 +59,110 @@ namespace GlanceBattery
         private void RateAndReviewMenuBarItem_OnClick(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/PageGroups/RateAndReviewGroup/RateAndReviewPage.xaml", UriKind.Relative));
+        }
+
+        private static readonly string IconicTileQuery = "tile=iconic";
+        public string Name = "Periodic Agent";
+
+        private void ApplicationBarIconButton_OnClick(object sender, EventArgs e)
+        {
+            Uri tileUri = new Uri(string.Concat("/MainPage.xaml?", IconicTileQuery), UriKind.Relative);
+            ShellTileData tileData = CreateIconicTileData();
+            ShellTile iconicTile = this.FindTile(IconicTileQuery);
+            if (iconicTile != null)
+            {
+                iconicTile.Delete();
+                ShellTile.Create(tileUri, tileData, true);
+                StartPeriodicAgent();
+            }
+            else
+            {
+                ShellTile.Create(tileUri, tileData, true);
+                StartPeriodicAgent();
+            }
+
+            ShellTile primaryTile = ShellTile.ActiveTiles.FirstOrDefault();
+            primaryTile.Update(tileData);
+        }
+
+        private ShellTileData CreateIconicTileData()
+        {
+            IconicTileData iconicTileData = new IconicTileData();
+            iconicTileData.Count = 99;
+            iconicTileData.IconImage = new Uri("/Assets/Icon/battery-medium.png", UriKind.Relative);
+            iconicTileData.SmallIconImage = new Uri("/Assets/Icon/battery-medium.png", UriKind.Relative);
+            iconicTileData.WideContent1 = "Your battery is at " + Battery.GetDefault().RemainingChargePercent +" %";
+            iconicTileData.WideContent2 = "Time remaining: " + ToReadableString(Battery.GetDefault().RemainingDischargeTime);
+
+            return iconicTileData;
+        }
+
+        private ShellTile FindTile(string partOfUri)
+        {
+            ShellTile shellTile = ShellTile.ActiveTiles.FirstOrDefault(
+            tile => tile.NavigationUri.ToString().Contains(partOfUri));
+
+            return shellTile;
+        }
+
+        public void StartPeriodicAgent()
+        {
+            periodicTask = ScheduledActionService.Find(Name) as PeriodicTask;
+
+            if (periodicTask != null)
+            {
+                RemoveAgent();
+            }
+
+
+            periodicTask = new PeriodicTask(Name);
+            periodicTask.Description = "this describes the periodic task";
+
+            try
+            {
+                ScheduledActionService.Add(periodicTask);
+                //LoadPeriodicTaskData();
+
+#if(DEBUG_AGENT)
+                ScheduledActionService.LaunchForTest(Name, TimeSpan.FromSeconds(30));
+#endif
+            }
+            catch (Exception e) { }
+        }
+
+        public void RemoveAgent()
+        {
+            try
+            {
+                ScheduledActionService.Remove(Name);
+                //EmptyPeriodicData();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        public static string ToReadableString(TimeSpan span)
+        {
+            string formatted = string.Format("{0}{1}{2}{3}",
+                span.Duration().Days > 0
+                    ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Hours > 0
+                    ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Minutes > 0
+                    ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Seconds > 0
+                    ? string.Format("{0:0} second{1}", span.Seconds, span.Seconds == 1 ? String.Empty : "s")
+                    : string.Empty);
+
+            if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+            if (string.IsNullOrEmpty(formatted)) formatted = "0 seconds";
+
+            return formatted;
         }
     }
 }
---
+++ GlanceBattery/UpdateBatteryPercent/ScheduledAgent.cs
@@ -0,0 +1,128 @@
+﻿#define DEBUG_AGENT
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Windows.Phone.Devices.Power;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
+
+namespace UpdateBatteryPercent
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+
+        private static int count = Battery.GetDefault().RemainingChargePercent;
+        private static readonly string IconicTileQuery = "tile=iconic";
+
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        static ScheduledAgent()
+        {
+            // Subscribe to the managed exception handler
+            Deployment.Current.Dispatcher.BeginInvoke(delegate
+            {
+                Application.Current.UnhandledException += UnhandledException;
+            });
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private static void UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            //TODO: Add code to perform your task in background
+            UpdatePrimaryTile(count, "%");
+            UpdateSecondaryTile(count);
+            count++;
+
+#if DEBUG_AGENT
+            ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(30));
+#endif
+            NotifyComplete();
+        }
+
+        public static void UpdateSecondaryTile(int count)
+        {
+            Uri tileUri = new Uri(string.Concat("/MainPage.xaml?", IconicTileQuery), UriKind.Relative);
+            ShellTileData tileData = CreateIconicTileData();
+            ShellTile iconicTile = FindTile(IconicTileQuery);
+            if (iconicTile != null)
+            {
+                iconicTile.Update(tileData);
+            }
+        }
+
+        public static void UpdatePrimaryTile(int count, string content)
+        {
+            //FlipTileData primaryTileData = new FlipTileData();
+            //primaryTileData.Count = count;
+            //primaryTileData.BackContent = content;
+
+
+            ShellTileData tileData = CreateIconicTileData();
+            ShellTile primaryTile = ShellTile.ActiveTiles.First();
+            primaryTile.Update(tileData);
+        }
+
+        private static ShellTileData CreateIconicTileData()
+        {
+            IconicTileData iconicTileData = new IconicTileData();
+            iconicTileData.Count = count;
+            iconicTileData.IconImage = new Uri("/Assets/Icon/battery-medium.png", UriKind.Relative);
+            iconicTileData.SmallIconImage = new Uri("/Assets/Icon/battery-medium.png", UriKind.Relative);
+            iconicTileData.WideContent1 = "Your battery is at " + count + " %";
+            iconicTileData.WideContent2 = "Time remaining: " + ToReadableString(Battery.GetDefault().RemainingDischargeTime);
+
+            return iconicTileData;
+        }
+
+        private static ShellTile FindTile(string partOfUri)
+        {
+            ShellTile shellTile = ShellTile.ActiveTiles.FirstOrDefault(
+            tile => tile.NavigationUri.ToString().Contains(partOfUri));
+
+            return shellTile;
+        }
+
+        public static string ToReadableString(TimeSpan span)
+        {
+            string formatted = string.Format("{0}{1}{2}{3}",
+                span.Duration().Days > 0
+                    ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Hours > 0
+                    ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Minutes > 0
+                    ? string.Format("{0:0} minute{1}, ", span.Minutes, span.Minutes == 1 ? String.Empty : "s")
+                    : string.Empty,
+                span.Duration().Seconds > 0
+                    ? string.Format("{0:0} second{1}", span.Seconds, span.Seconds == 1 ? String.Empty : "s")
+                    : string.Empty);
+
+            if (formatted.EndsWith(", ")) formatted = formatted.Substring(0, formatted.Length - 2);
+
+            if (string.IsNullOrEmpty(formatted)) formatted = "0 seconds";
+
+            return formatted;
+        }
+    }
+}
